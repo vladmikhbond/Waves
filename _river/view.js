a42b4232b50085d13fdd0833430067456fcb2e23
volodymyr bondariev <vladmikhbond@hotmail.com>
2019-01-31 13:15:17 +0200

    error about W fixed in 1D

--- _river/view.js
+++ _river/view.js
@@ -8,12 +8,15 @@ class View
 
 
     draw () {
+        const V_SCALE = 30;
         let n05 = this.n / 2 | 0;
 
         let ctx = canvas1.getContext('2d');
         ctx.clearRect(0, 0, this.n, this.n);
         ctx.strokeStyle = 'gray';
         ctx.lineWidth = 0.5;
+        ctx.setLineDash([5, 3]);
+
 
         ctx.beginPath();
         // Ox axis
@@ -23,21 +26,21 @@ class View
         // osc.ampl lines
         let osc = this.river.oscs[0];
         if (osc) {
-            let h = n05 - osc.ampl * 30
+            let h = n05 - osc.ampl * V_SCALE;
             ctx.moveTo(0, h); ctx.lineTo(this.n, h);
-            h = n05 + osc.ampl * 30
+            h = n05 + osc.ampl * V_SCALE;
             ctx.moveTo(0, h); ctx.lineTo(this.n, h);
         }
-
         ctx.stroke();
 
+        ctx.setLineDash([]);
         ctx.lineWidth = 1;
         ctx.strokeStyle = 'red';
         ctx.beginPath();
         for (let x = 0; x < opts.N; x++) {
             let h = this.river.w[x].x;
             ctx.moveTo(x, n05);
-            ctx.lineTo(x, n05 + 30 * h);
+            ctx.lineTo(x, n05 + V_SCALE * h);
         }
         ctx.stroke();
 
